991466681528ead48b161ee80e99b6dbe99e967d
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2025-08-13 16:53:00 -0300

    .

--- Aula11-CondicoesEncadeadas/exemploJS/media.js
+++ Aula11-CondicoesEncadeadas/exemploJS/media.js
@@ -6,11 +6,18 @@ let media = (nota1 + nota2) / 2
 if (media >= 70) {
     console.log("APROVADO");
 } 
-
 else if (media >= 50) {
+    
     console.log("RECUPERAÇÃO");
+    let recuperacao = 90
+    let mediaFinal = (media + recuperacao) / 2
+    
+    if (mediaFinal >= 70) {
+        console.log("APROVADO");
+    } else {
+        console.log("REPROVADO");
+    }
 } 
-
 else {
     console.log("REPROVADO");
 }
